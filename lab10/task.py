e7e97dcad9429d49ad2ce9617438e348974ec178
ishtiiyak <139499812+ishtiiyak@users.noreply.github.com>
2024-09-12 13:26:28 +0500

    lab 10

---
+++ lab10/task.py
@@ -0,0 +1,71 @@
+# sumEven =0
+# sumOdd =0
+# for i in range(10):
+#     num=eval(input('enter a number   '))
+#     if(num%2==0):
+#         sumEven+=num
+#     elif(num%2!=0):
+#        sumOdd+=num
+
+
+# print(f'sum of even is  {sumEven} and odd is {sumOdd}')
+
+
+
+
+# sum=0
+
+# for i in range(5):
+#     subMark=eval(input(f'enter sub {i+1}  '))
+#     sum+=subMark
+
+# print(f'sum mean {sum/5}')
+
+
+
+
+
+# num=eval(input(f'enter a number  '))
+# sum=0
+# if(num%2==0):
+#     for i in range(2,num+1,2):
+#         sum+=i
+        
+# elif(num%2!=0):
+#     for i in range(1,num+1,2):
+#         sum+=i
+    
+# print(sum)
+
+
+
+
+
+# num=eval(input(f'enter a number  '))
+# fact=1
+
+# for i in range(1,num+1):
+#     fact*=i   
+# print(fact)
+
+
+
+
+
+
+# num=eval(input(f'enter a number  '))
+
+
+
+# for i in range(2,num):
+#     if(num%i==0):
+       
+#         print('not')
+#         break
+          
+# else:
+#     print('yes')
+
+
+
+
