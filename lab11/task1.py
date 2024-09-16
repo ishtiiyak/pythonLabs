0baf13983a03c0c9b74871f0405cbc7e2148bc79
ishtiiyak <139499812+ishtiiyak@users.noreply.github.com>
2024-09-16 20:14:39 +0500

    done task 2 and 3

--- lab11/task1.py
+++ lab11/task1.py
@@ -15,3 +15,38 @@ for i in range(3):
         product*=x
 
 print(f'sum of all numbers is {sum} and product is {product}')
+
+##############################
+########### TASK 2 ###########
+##############################
+x=eval(input('enter a number'))
+sum=0
+for i in range(1,x+1):
+    fact=1
+    for j in range(1,i+1):
+        fact*=j
+    sum+=(1/fact)
+print(sum)
+
+##############################
+########### TASK 3 ###########
+##############################
+def fact(num):
+    fact=1
+    for j in range(1,num+1):
+        fact*=j
+    return fact
+    
+def mySeries(num):
+    sum=0
+    for i in range(1,num+1):
+        sum+=1/fact(i)
+    return sum
+        
+print(mySeries(x))      
+        
+        
+
+
+
+
