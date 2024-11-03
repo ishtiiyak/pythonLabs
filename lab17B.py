73fedc2e0adda584b49122f8d38a03ff95c195a5
Ishtiiyak <ishtiiyak@gmail.com>
2024-11-03 22:10:44 +0500

    fibnnochi

--- lab17B.py
+++ lab17B.py
@@ -41,3 +41,18 @@ sorted_numbers = sorted(numbers,reverse=True)
 
 # Printing the sorted list
 print(sorted_numbers,len(numbers))
+#write python code for fibnocchi
+def fibonacci(n):
+    if n <= 0:
+        return "Input should be positive integer."
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        a, b = 0, 1
+        for _ in range(n - 2):
+            a, b = b, a + b
+            return b
+print(fibonacci(10))
+        
