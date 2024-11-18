19336a0bb48ec44582a529a8563bcce42f6c59db
Ishtiiyak <ishtiiyak@gmail.com>
2024-11-19 02:28:16 +0500

    new commits

--- MUcourse/files.py
+++ MUcourse/files.py
@@ -1,3 +1,8 @@
 fhandle = open(r'C:\Users\BUZZ TECH\pythonLabs\MUcourse\txt\file.txt', 'r')
-for line in fhandle:
-    print(line)
+# for line in fhandle:
+#     print(line,end="")
+
+
+fhandle.seek(0)
+lines = fhandle.readlines()
+print("Read lines as list:\n", lines)
