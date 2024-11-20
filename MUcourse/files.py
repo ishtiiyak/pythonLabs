ffea8b64fda8a21c3b5bb5a3c34043c11da6beb4
Ishtiiyak <ishtiiyak@gmail.com>
2024-11-20 21:14:26 +0500

    new file generated

--- MUcourse/files.py
+++
@@ -1,8 +0,0 @@
-fhandle = open(r'C:\Users\BUZZ TECH\pythonLabs\MUcourse\txt\file.txt', 'r')
-# for line in fhandle:
-#     print(line,end="")
-
-
-fhandle.seek(0)
-lines = fhandle.readlines()
-print("Read lines as list:\n", lines)
