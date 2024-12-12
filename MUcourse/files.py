65a75d9022fdb2a5f8373205e97a3290897d3f13
Ishtiiyak <ishtiiyak@gmail.com>
2024-12-12 10:38:53 +0000

    Dec 12, 2024, 3:38 PMjjj

--- MUcourse/files.py
+++ MUcourse/files.py
@@ -1,4 +1,4 @@
-fhandle = open(r'C:\Users\BUZZ TECH\pythonLabs\MUcourse\txt\files.txt', 'r')
+fhandle = open(r'C:\Users\BUZZ TECH\pythonLabs\MUcourse\txt\files.txt', 'r
 for line in fhandle:
     #line=line.rstrip()
     if not line.startswith('From'):
