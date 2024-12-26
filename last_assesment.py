0257d6374ba76dcc7be1c0510718182d8d07007e
ishtiiyak <139499812+ishtiiyak@users.noreply.github.com>
2024-12-26 15:18:44 +0500

    Update last_assesment.py

--- last_assesment.py
+++ last_assesment.py
@@ -31,12 +31,14 @@ class OvernightPackage(Package):
         return self.weight * (self.cost_per_ounce + self.fee_per_ounce)
 
 
-
 package1 = Package("Ahmad", "110", "Lahore", "Punjab", "5001", "1200", 12, 3)
 print("Package1 Cost:", package1.calculate_cost())
+print(f"City: {package1.city}, State: {package1.state}, ZIP: {package1.ZIPcodeSender} -> {package1.ZIPcodeReceiver}")
 
 package2 = TwoDayPackage("Ali", "2201", "Karachi", "Sindh", "8000", "9000", 6, 7, 5)
 print("TwoDayPackage Cost:", package2.calculate_cost())
+print(f"City: {package2.city}, State: {package2.state}, ZIP: {package2.ZIPcodeSender} -> {package2.ZIPcodeReceiver}")
 
-package3 = OvernightPackage("Sara", "110", "nagar", "gb", "12000", "5000", 8, 4, 2)
+package3 = OvernightPackage("Sara", "110", "Nagar", "GB", "12000", "5000", 8, 4, 2)
 print("OvernightPackage Cost:", package3.calculate_cost())
+print(f"City: {package3.city}, State: {package3.state}, ZIP: {package3.ZIPcodeSender} -> {package3.ZIPcodeReceiver}")
